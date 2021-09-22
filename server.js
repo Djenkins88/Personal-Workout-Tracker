3b734571fdbb4a87ecb4e2d2d387f68472087e82
Dominque Jenkins <dominquejenkins14@gmail.com>
2021-09-21 22:50:17 -0500

    added app/routes/apiRoutes.js to server file

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 app.use(logger("dev"));
 
 // routes
-app.use(require("./routes/apiRoutes.js"));
+app.use(require(".app/routes/apiRoutes.js"));
 app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
